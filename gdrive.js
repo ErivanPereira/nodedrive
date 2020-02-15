38e58474034913d5b07c4a8e1dec77be280c3189
ErivanPereira <34924779+ErivanPereira@users.noreply.github.com>
2020-02-15 20:48:46 -0300

    Update gdrive.js

--- gdrive.js
+++ gdrive.js
@@ -1,32 +1,6 @@
 const fs = require("fs");
 const {google} = require('googleapis');
 
-/*function imageDownload(id){
-  require("./gdrive-auth")((auth) => {
-    const drive = google.drive({version: 'v3', auth});
-
-    drive.files.get(
-      {fileId: id, alt: 'media'}, 
-      {responseType: 'stream'},
-      function(err, { data }) {
-        if (err) {
-          reject("The API returned an error: " + err);
-        }
-        let buf = [];
-        data.on("data", function(e) {
-          buf.push(e);
-        });
-        data.on("end", function(){
-          const buffer = Buffer.concat(buf);
-          fs.writeFile('/tmp/photo.jpg', buffer, err => {} );
-          
-          callback(file.data.id);
-        });
-      }
-    );
-  })
-}*/
-
 function imageDownload(id){
   return "https://lh3.google.com/u/0/d/" + id
 }
